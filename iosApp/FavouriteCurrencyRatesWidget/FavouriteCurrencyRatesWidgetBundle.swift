dd288f019d52d02ee3e782461f30e649b02348a0
aleks100s <aleksandr100s@yandex.ru>
2025-09-09 22:07:24 +0700

    Initialize iOS widget

---
+++ iosApp/FavouriteCurrencyRatesWidget/FavouriteCurrencyRatesWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  FavouriteCurrencyRatesWidgetBundle.swift
+//  FavouriteCurrencyRatesWidget
+//
+//  Created by Alexander on 09.09.2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct FavouriteCurrencyRatesWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        FavouriteCurrencyRatesWidget()
+    }
+}
